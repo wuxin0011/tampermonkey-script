95535748c42dd12475562ba2e2b188540a154e55
wuxin001 <2191377759@qq.com>
2023-09-30 10:21:29 +0800

    feat:优化read.dark

--- live-plugin/src/style/dark/bilibili/read.js
+++ live-plugin/src/style/dark/bilibili/read.js
@@ -17,20 +17,22 @@ const read = /.*:\/\/.*\.bilibili\.com\/read\/.*/.test(local_url) ?
 .dark .normal-article-holder h6,
 .dark .article-container .title-container .title,
 .dark .article-breadcrumb .breadcrumb-title,
-.dark .article-read-info>span[data-v-36aefa22],.dark .article-read-info>span,
-.dark .right-side-bar .to-top .iconfont[data-v-38a9fd1d],.dark .right-side-bar .to-top .iconfont,
-.dark .right-side-bar .side-toolbar .toolbar-item[data-v-38a9fd1d],.dark .right-side-bar .side-toolbar .toolbar-item,
+.dark [class^=article-read-info]>span,
+.dark .right-side-bar .to-top [class^=iconfont],
+.dark .right-side-bar .side-toolbar [class^=toolbar-item],
 .dark .coin-dialog-wrapper .coin-title,.dark .coin-dialog-wrapper .van-icon-guanbi,
-.dark .right-side-bar .side-toolbar .toolbar-item .iconfont[data-v-38a9fd1d],.dark .right-side-bar .side-toolbar .toolbar-item .iconfont
- {
+.dark .right-side-bar .side-toolbar .toolbar-item [class^=iconfont]
+
+{
   color:var(--w-text-light) !important;
 }
 
-.dark .right-side-bar .to-top[data-v-38a9fd1d],.dark .right-side-bar .to-top,
-.dark .right-side-bar .side-toolbar[data-v-38a9fd1d],.dark .right-side-bar .side-toolbar,
-.dark .follow-btn[data-v-2847c980],.dark .follow-btn,
+.dark .right-side-bar [class^=to-top],
+.dark .right-side-bar [class^=side-toolbar],
+.dark [class^=follow-btn],
 .dark .coin-dialog-wrapper .confirm-btn,
 .dark .van-popover.van-followed,
+.dark .right-side-bar [class^=catalog],
 .dark .coin-dialog-wrapper,
 .dark .article-container {
   border: 1px solid var(--w-border) !important;
@@ -40,19 +42,19 @@ const read = /.*:\/\/.*\.bilibili\.com\/read\/.*/.test(local_url) ?
 
 .dark .article-container ,
 .dark .comment-wrapper .comment-m,
-.dark .followed[data-v-2847c980],.dark .followed,
-.dark .article-up-info[data-v-904253a6],.dark .article-up-info,
+.dark [class^=followed],
+.dark [class^=article-up-info],
+.dark .right-side-bar [class^=catalog]::after,
 .dark .fixed-top-header {
   color:var(--w-text-light) !important;
   background:var(--w-bg-darker) !important;
 }
 
 .dark .nav-tool-container .section:hover,
-.dark .right-side-bar .to-top .iconfont[data-v-38a9fd1d]:hover,.dark .right-side-bar .to-top .iconfont:hover,
-.dark .right-side-bar .to-top[data-v-38a9fd1d]:hover,.dark .right-side-bar .to-top:hover,
+.dark .right-side-bar .to-top [class^=iconfont]:hover,
+.dark .right-side-bar [class^=to-top]:hover,
 .dark .coin-dialog-wrapper .confirm-btn:hover,
-
-.dark .nav-tool-container .section[data-v-3b26ecb6]:hover {
+.dark .nav-tool-container [class^=section]:hover {
   color:var(--w-blue-link-hover) !important;
   border-color: var(--w-blue-link-hover) !important;
   background:var(--w-bg-darker) !important;
