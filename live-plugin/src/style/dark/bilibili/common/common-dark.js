b11b642b5da38edf8a663d54bfafa7822cb2be57
wuxin001 <2191377759@qq.com>
2023-10-02 19:06:48 +0800

    add:common.dark

---
+++ live-plugin/src/style/dark/bilibili/common/common-dark.js
@@ -0,0 +1,13 @@
+
+
+const commonDark= `
+.dark * {
+  color:var(--w-text-light) !important;
+  border-color: var(--w-border) !important;
+  background-color:var(--w-bg-darker) !important;
+  outline-color: var(--w-border) !important;
+}
+
+`
+
+export default commonDark
